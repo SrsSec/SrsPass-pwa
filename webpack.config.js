0e9f73e39d89d8268469bb3a3f7cf68a428a4ded
D-Nice <d-nice2@protonmail.com>
2020-11-28 18:48:09 -0500

    refactor!: project structure for pwa & intuitivity
    actually use the dependencies imported in last commits, which were needed for pwa but also general improved intuitivity of the project structure, by moving old /public under /src tree and instead utilizing dist/ for the build now.
    
    Essentially, we are letting webpack do its job, and handle the bundling of all src files and built files in dist, rather than pollution between static and built files in public. The previous way may in some respects have been easier and tighter from a certain security perspective, from signing the files, and having a purely static index html file, among others.

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,58 @@
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
+const { IgnorePlugin } = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 const mode = process.env.NODE_ENV || 'development';
 const prod = mode === 'production';
+
+const WorkboxServiceWorker = new WorkboxPlugin.GenerateSW({
+  //include: [path.resolve(__dirname, 'public')],
+  include: [],
+  additionalManifestEntries: [
+    // consider precaching this when stable
+    //'/',
+    {
+      url: '/argon2id.worker.js',
+      // TODO update this with a real subhash or rev number, should stay pretty static though
+      revision: '1337',
+    },
+  ],
+  runtimeCaching: [
+    {
+      urlPattern: /\.png$/,
+      handler: 'CacheFirst',
+    },
+    {
+      urlPattern: '/index.html',
+      handler: 'CacheFirst',
+    },
+    {
+      urlPattern: '/',
+      handler: 'CacheFirst',
+    },
+    {
+      urlPattern: '/bundle.js',
+      handler: 'StaleWhileRevalidate',
+    },
+    {
+      urlPattern: /\.css$/,
+      handler: 'StaleWhileRevalidate',
+    },
+    {
+      urlPattern: /\.js$/,
+      handler: 'StaleWhileRevalidate',
+    },
+  ],
+  maximumFileSizeToCacheInBytes: 5000000,
+  // these options encourage the ServiceWorkers to get in there fast
+  // and not allow any straggling "old" SWs to hang around
+  clientsClaim: true,
+  skipWaiting: true,
+})
+
 
 module.exports = {
   node: {
@@ -15,6 +65,8 @@ module.exports = {
 		alias: {
 			svelte: path.resolve('node_modules', 'svelte'),
 			'@': path.resolve(__dirname, 'src'),
+			'@root': path.resolve(__dirname, ''),
+			'@public': path.resolve(__dirname, 'src/public'),
 			'@component': path.resolve(__dirname, 'src/component'),
 			'@modal': path.resolve(__dirname, 'src/modal'),
 			'@worker': path.resolve(__dirname, 'src/worker'),
@@ -25,7 +77,7 @@ module.exports = {
 		mainFields: ['svelte', 'browser', 'module', 'main']
 	},
 	output: {
-		path: __dirname + '/public',
+		path: __dirname + '/dist',
 		filename: '[name].js',
 		chunkFilename: '[name].[id].js'
 	},
@@ -63,6 +115,18 @@ module.exports = {
         //
         // Error: WebAssembly module is included in initial chunk.
         type: 'javascript/auto'
+      },
+      {
+        include: /public\//,
+        test: /\.webmanifest$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]'
+            }
+          }
+        ]
       }
     ]
 	},
@@ -70,7 +134,18 @@ module.exports = {
 	plugins: [
 		new MiniCssExtractPlugin({
 			filename: '[name].css'
-		})
+		}),
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'src/public/index.html',
+      favicon: 'src/public/favicon.png',
+      files: {
+        manifest: 'src/public/manifest.webmanifest',
+        css: 'src/public/global.css',
+      }
+    }),
+    new IgnorePlugin(/^\.\/wordlists\/(?!english)/, /bip39\/src$/),
+    ...(prod ? [WorkboxServiceWorker] : []),
 	],
-	devtool: prod ? false: 'source-map'
+	devtool: prod ? false : 'source-map'
 };
