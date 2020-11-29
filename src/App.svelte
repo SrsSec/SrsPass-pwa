f78c2b4849637e8fce5cde5dcbdc2114b6f85aad
D-Nice <d-nice2@protonmail.com>
2020-11-28 19:07:35 -0500

    feat: add footer & public imports to App
    adding footer should be quite clear. Due to the webpack refactor, we now have to import the necessary public files in our top-level App file to have them bundled by webpack.

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,31 @@
 <script>
-  import Content from './Content.svelte'
+import '@public/global.css'
+import '@public/manifest.webmanifest'
+import Content from './Content.svelte'
+import Footer from '@component/Footer.svelte'
 </script>
 
-<Content />
+<style>
+.page-container {
+  position: relative;
+  min-height: 100vh;
+}
+
+.content-wrap {
+  padding-bottom: 3rem;    /* At Least Footer height */
+}
+
+:global(footer) {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  height: 2.5rem;            /* Footer height */
+}
+</style>
+
+<div class="page-container">
+  <div class="content-wrap">
+    <Content />
+  </div>
+  <Footer />
+</div>
