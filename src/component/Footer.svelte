9390dd8fcaafcacb785c48c91344d9df726a960e
D-Nice <d-nice2@protonmail.com>
2020-11-28 19:01:12 -0500

    feat: add footer component

---
+++ src/component/Footer.svelte
@@ -0,0 +1,7 @@
+<script>
+// TODO consider security implications of importing package.json
+import { version } from '@root/package.json'
+</script>
+<footer>
+Copyright 2020 - SrsPass by SrsSec - v{version}
+</footer>
