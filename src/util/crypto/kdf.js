f79ae994658b89041afefbfdb719524f50bb23ab
D-Nice <d-nice2@protonmail.com>
2021-01-18 22:32:19 -0500

    feat: extract kdf code into own file
    first step in the process of dividing the crypto code for more logical organization of their respective functionalities

---
+++ src/util/crypto/kdf.js
@@ -0,0 +1,51 @@
+import WorkerArgon2id from '@worker/argon2id.worker.js'
+
+// t 12 m 15 takes about 5 secs
+// t 13 m 14 takes about 2 secs
+export const a2params = {
+  heavy: {
+    time: 12,
+    mem: (1 << 15)
+  },
+  light: {
+    time: 16,
+    mem: (1 << 12)
+  },
+  test: {
+    time: 19,
+    mem: (1 << 9)
+  }
+}
+
+// promisified interface to the argon2id worker
+// creates ephemeral instances per call
+// this has the benefit of allowing parallel runs for different hashes
+export function argon2(pass, salt, opt = {}) {
+  return new Promise(resolve => {
+    const worker = new WorkerArgon2id()
+    const params = { salt, ...opt }
+    if (!params.hashLen) params.hashLen = 32
+    worker.onmessage = ({ data }) => {
+      worker.terminate()
+      resolve(data)
+    }
+    worker.postMessage({ pass, params })
+  })
+}
+
+// unlike bip39, we derive using custom argon2 params instead of pbkdf2
+// TODO put in encryption
+const deriveMnemonicSeed = async (mnemonic) =>
+  await getArgon2Hash(mnemonic, 'SrsPass seed', a2params.heavy)
+
+// for the most part will be working with Buffer types
+// so this helper function helps avoid redundant await code
+export const getArgon2Hash = async (pass, salt, opt) =>
+  (await argon2(pass, salt, opt)).res.hash
+
+// TODO put in encryption
+const deriveSeedEncryptionKey = async (pass, salt) =>
+  await getArgon2Hash(pass, salt, a2params.heavy)
+
+export const deriveGeneratorPassword = async pass =>
+  await getArgon2Hash(pass, 'SrsPass generator v0', a2params.heavy)
