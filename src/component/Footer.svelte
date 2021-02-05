3491d10ecab47cefe5b57f22d2fcf040cfa8c292
D-Nice <d-nice2@protonmail.com>
2021-02-04 19:50:56 -0500

    feat: linkify commit hash in footer to source code

--- src/component/Footer.svelte
+++ src/component/Footer.svelte
@@ -2,12 +2,13 @@
 // TODO consider security implications of importing package.json
 import { version } from '@root/package.json'
 const currentYear = new Date().getFullYear()
-  , shortHash = process.env.GIT_HASH.substr(0,4)
+  , commitHash = process.env.GIT_HASH
+  , shortHash = commitHash.substr(0,4)
   , email = 'srspass@srssec.com'
 </script>
 <footer>
   &copy; {currentYear} -
   <a target="_blank" href="https://docs.srspass.com"> docs</a> -
   <a href={"mailto:" + email}> contact</a> -
-  v{version}-{shortHash}
+  v{version}-<a href={"https://github.com/SrsSec/SrsPass-pwa/tree/" + commitHash}>{shortHash}</a>
 </footer>
