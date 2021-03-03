00288d9740523488d4aadabbf3d31b6983f2d359
D-Nice <d-nice2@protonmail.com>
2021-03-02 21:26:06 -0500

    chore(release): creates auto signoff script
    the script is setup in such a way to rm the need for any user prompts. If successful it also selfdestructs/rm itself. By wrapping EOF tag in single quotes, we avoid execution of any of the shell parameters within the string, without it they execute and could lead to unwanted removal of the release script etc...

--- .github/release.sh
+++ .github/release.sh
@@ -7,4 +7,18 @@ fi
 # supports passing of additional args to standard-version
 npx standard-version@7.1.0 "$FIRST" "$@" --header '# CHANGELOG
 ' \
-  && echo "Sign-off commit and tags via 'git commit --amend --signoff' and 'git tag -sf v...'"
+  && echo "Sign-off commit and tags via 'git commit --amend --signoff' and 'git tag -sf v...'" \
+  && echo "Generating auto-script to do this for you, just run './signoff'" \
+  && cat > signoff <<'EOF'
+#!/bin/sh
+echo "Running git commands to sign off the last release commit and tag..."
+git commit --amend --signoff --no-edit \
+  && git tag -sfm "$(git log --oneline -n1 | tail -n1)" $(git tag | tail -n1) \
+  && echo "DONE signoff" \
+  && rm "$0" \
+  && exit 0
+echo "something went wrong... rm this script"
+echo "you may have to signoff manually if you didn't get a DONE msg"
+exit 1
+EOF
+chmod +x signoff
