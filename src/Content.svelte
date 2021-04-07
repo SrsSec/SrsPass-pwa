d841d3065852c0a07279fa672ef910bd7af16385
D-Nice <d-nice2@protonmail.com>
2021-04-06 21:08:49 -0400

    feat: make initial components dynamic imports for lazy-load
    this is the initial lazy load commit, which should greatly increase first paint/load speed for users where the UI becomes usable, even for very slow internet devices
    
    quite a bit of cleanup done with rming unused modules/lines
    
    also adds a super lightweight spinner component

--- src/Content.svelte
+++ src/Content.svelte
@@ -1,14 +1,23 @@
 <script>
-  import { getContext } from 'svelte'
-  import { isNil } from 'ramda'
   import Title from '@component/Title.svelte'
-  import FirstVisitSetup from '@component/Setup.svelte'
-  import PassGenerator from '@component/PassGenerator.svelte'
-  import * as constant from '@/constants.js'
+  import LoadingSpinner from '@component/LoadingSpinner.svelte'
+  import { onMount } from 'svelte'
 
-  localStorage.removeItem('loglevel:webpack-dev-server')
+  let FirstVisitSetup
+    , PassGenerator
+
+  onMount(async () => {
+    import('@component/PassGenerator.svelte')
+      .then(c => PassGenerator = c.default)
+    import('@component/Setup.svelte')
+      .then(c => FirstVisitSetup = c.default)
+  })
 </script>
 
-<Title/>
-<FirstVisitSetup/>
-<PassGenerator/>
+<Title />
+<svelte:component this={FirstVisitSetup} />
+{#if PassGenerator}
+  <svelte:component this={PassGenerator} />
+{:else}
+  <LoadingSpinner />
+{/if}
