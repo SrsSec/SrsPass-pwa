aeaa383f1d2f97c60c6d6b5131412fc7cc259d7b
D-Nice <d-nice2@protonmail.com>
2021-05-04 22:32:23 -0400

    refactor: improve setup flow radio layout
    with this change, the radio btn and related label should stay on the same lines, rather than possibly being wrapped on different lines for small screens

--- src/modal/firstVisit/SetupMsg.svelte
+++ src/modal/firstVisit/SetupMsg.svelte
@@ -55,11 +55,9 @@ function handleKeydown(evt) {
 <svelte:window on:keydown={handleKeydown} />
 
 <p>Please select your setup option!</p>
-<div id="setup-radio-group">
-  <input type="radio" id="radio1" bind:group={setupOption} value={'full'}><label for="radio1" title="Recommended setup for new users.">Full Setup{@html tt}</label>
-  <input type="radio" id="radio2" bind:group={setupOption} value={'quick'}><label for="radio2" title="Minimal setup prompts to get new users on SrsPass.">Quick Setup</label>
-  <input type="radio" id="radio3" bind:group={setupOption} value={'existing'}><label for="radio3" title="For existing users that have a backup phrase to import. &#013;Backup phrases are crypto BIP39 12/18/24 mnemonics, new users with crypto wallets can use this step.">Existing User</label>
-</div>
+<div class="setup-radio-line"><input type="radio" id="radio1" bind:group={setupOption} value={'full'}><label for="radio1" title="Recommended setup for new users.">Full Setup{@html tt}</label></div>
+<div class="setup-radio-line"><input type="radio" id="radio2" bind:group={setupOption} value={'quick'}><label for="radio2" title="Minimal setup prompts to get new users on SrsPass.">Quick Setup{@html tt}</label></div>
+<div class="setup-radio-line"><input type="radio" id="radio3" bind:group={setupOption} value={'existing'}><label for="radio3" title="For existing users that have a backup phrase to import. &#013;Backup phrases are crypto BIP39 12/18/24 mnemonics, new users with crypto wallets can use this step.">Existing User</label></div>
 <p>In the next steps, we'll generate your 12-word {@html mnemonicHtml} and store it safely encrypted in your browser.</p>
 <p>Think of it as a secret account ID, like a SIN, <strong style="text-transform: uppercase">save a backup for yourself and keep it secure.</strong></p>
 <p>Seriously, write it in a notebook at least and securely on your phone, save multiple copies. You will need it to sync other devices or recover your passwords in the future.</p>
@@ -67,10 +65,10 @@ function handleKeydown(evt) {
 <!--><p><i>This phrase doubles as a crypto wallet mnemonic, as it's bip39. In fact, if you like
     you may import your own phrase, which we'll prompt you for!</i></p>-->
 <style>
-  #setup-radio-group {
+  .setup-radio-line {
     display: inline-block;
   }
-  #setup-radio-group label {
+  .setup-radio-line label {
     display: unset;
     margin-left: 0.5em;
     margin-right: 1em;
